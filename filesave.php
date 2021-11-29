6c757080328d98ba57be54ac931d8b7276b09d13
caffeine <caffeine.konchan@gmail.com>
2021-11-30 01:06:14 +0900

    add fileupload

---
+++ filesave.php
@@ -0,0 +1,23 @@
+<?php
+putenv("GOOGLE_APPLICATION_CREDENTIALS=my-first-project.json");
+require __DIR__ . '/vendor/autoload.php';
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+
+//filenameの受け渡しができていなく画像がないといわれている
+$filename = isset($_FILES["upload_image"]["name"]) ? $_FILES["upload_image"]["name"] : "";
+echo $filename;
+//$upload_file = "/".$filename;
+$imageAnnotator = new ImageAnnotatorClient();
+$image = @file_get_contents(__DIR__ . $filename);// ここにファイル名
+$response = $imageAnnotator->labelDetection($image);
+$labels = $response->getLabelAnnotations();
+ 
+if ($labels) {
+    echo("Labels:" . PHP_EOL);
+    foreach ($labels as $label) {
+        echo($label->getDescription() . PHP_EOL);
+    }
+} else {
+    echo('No label found' . PHP_EOL);
+}
+?>
--- index.php
+++ index.php
@@ -1,19 +1,11 @@
-<?php
-putenv("GOOGLE_APPLICATION_CREDENTIALS=my-first-project.json");
-require __DIR__ . '/vendor/autoload.php';
-use Google\Cloud\Vision\V1\ImageAnnotatorClient;
-
-$imageAnnotator = new ImageAnnotatorClient();
-$image = file_get_contents(__DIR__ . '/Fotolia_71261977_M.jpg');// ここにファイル名
-$response = $imageAnnotator->labelDetection($image);
-$labels = $response->getLabelAnnotations();
- 
-if ($labels) {
-    echo("Labels:" . PHP_EOL);
-    foreach ($labels as $label) {
-        echo($label->getDescription() . PHP_EOL);
-    }
-} else {
-    echo('No label found' . PHP_EOL);
-}
-?>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <form action="filesave.php" method="post" enctype="multipart/form-data">
+        <p>ファイル：<input type="file" name="upload_image" size="40" /></p>
+        <p><input type="submit" value="送信" /></p>
+    </form>
+</body>
+</html>
