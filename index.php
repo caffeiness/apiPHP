40247a69e21d7108d09e6c0ce856b37715e97780
caffeine <caffeine.konchan@gmail.com>
2021-12-15 23:40:06 +0900

    fix index

--- index.php
+++ index.php
@@ -7,36 +7,105 @@ $json = json_decode($json,true);
 $channel_access_token = $json["channel_access_token"];
 $channel_secret = $json["channel_secret"];
 
-require_once('tick.php');
+$accessToken = $channel_access_token; 
+$jsonString = file_get_contents('php://input'); error_log($jsonString); 
+$jsonObj = json_decode($jsonString); $message = $jsonObj->{"events"}[0]->{"message"}; 
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text']
-                            )
-                        )
-                    ));
-                    break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
-    }
-};
 
+ // 送られてきたメッセージの中身からレスポンスのタイプを選択 
+if ($message->{"text"} == '確認') {
+     // 確認ダイアログタイプ 
+    $messageData = [ 
+        'type' => 'template', 
+        'altText' => '確認ダイアログ', 
+        'template' => [ 'type' => 'confirm', 'text' => '元気ですかー？', 
+            'actions' => [
+                [ 'type' => 'message', 'label' => '元気です', 'text' => '元気です' ],
+                [ 'type' => 'message', 'label' => 'まあまあです', 'text' => 'まあまあです' ], 
+            ] 
+        ]
+ ]; 
+} elseif ($message->{"text"} == 'ボタン') { 
+    // ボタンタイプ 
+    $messageData = [ 
+        'type' => 'template',
+         'altText' => 'ボタン', 
+        'template' => [
+             'type' => 'buttons',
+             'title' => 'タイトルです',
+             'text' => '選択してね', 
+            'actions' => [
+                 [ 
+                    'type' => 'postback', 
+                    'label' => 'webhookにpost送信', 
+                    'data' => 'value' 
+                ],
+                 [
+                     'type' => 'uri',
+                     'label' => 'googleへ移動', 
+                     'uri' => 'https://google.com' 
+                 ]
+              ]
+          ] 
+     ]; 
+} elseif ($message->{"text"} == 'カルーセル') {
+     // カルーセルタイプ 
+    $messageData = [ 
+        'type' => 'template', 
+        'altText' => 'カルーセル', 
+        'template' => [
+             'type' => 'carousel', 
+            'columns' => [ 
+                [ 
+                    'title' => 'カルーセル1', 
+                    'text' => 'カルーセル1です',
+                     'actions' => [
+                         [
+                            'type' => 'postback',
+                             'label' => 'webhookにpost送信',
+                             'data' => 'value'
+                         ],
+                         [ 
+                            'type' => 'uri', 
+                            'label' => '美容の口コミ広場を見る',
+                             'uri' => 'https://report.clinic/'
+                         ] 
+                    ] 
+                ],
+                 [ 
+                        'title' => 'カルーセル2', 
+                        'text' => 'カルーセル2です', 
+                        'actions' => [ 
+                            [
+                                'type' => 'postback', 
+                                'label' => 'webhookにpost送信', 
+                                'data' => 'value' 
+                            ], 
+                            [ 
+                                'type' => 'uri', 
+                                'label' => '女美会を見る', 
+                                'uri' => 'https://jobikai.com/' 
+                            ] 
+                        ] 
+                    ], 
+                ] 
+            ] 
+    ];
+ } else {
+     // それ以外は送られてきたテキストをオウム返し
+     $messageData = [ 'type' => 'text', 'text' => $message->{"text"} ]; 
+} 
+$response = [ 'replyToken' => $replyToken, 'messages' => [$messageData] ]; 
+error_log(json_encode($response)); 
+$ch = curl_init('https://api.line.me/v2/bot/message/reply'); 
+curl_setopt($ch, CURLOPT_POST, true); 
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response)); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json; charser=UTF-8', 'Authorization: Bearer ' . $accessToken )); 
+$result = curl_exec($ch); error_log($result); 
+curl_close($ch);
 ?>
 <!-- <html>
 <head>
