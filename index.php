ff86a56fcf6bacdc23b05b2719c4b79a5619e84b
caffeine <caffeine.konchan@gmail.com>
2021-12-19 00:32:07 +0900

    add css

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet">
 </head>
 <body>
+<div class="glass-container" id="glass">
     <form action="sample.php" method="post" enctype="multipart/form-data">
-        <p>ファイル：<input type="file" name="upload_image"></p>
+        <div class="file_button">
+            <p><span>ファイルを選択</span><input class="file_input" type="file" name="upload_image"></p>
+        </div>
         <p><input type="submit" value="送信"></p>
     </form>
+</div>
 </body>
 </html>
--- sample.php
+++ sample.php
@@ -33,6 +33,7 @@ echo $resultArr[0];
 <html>
 <head lang="ja">
   <title></title>
+  <link href="style.css" rel="stylesheet">
 
 </head>
   <body>
@@ -40,5 +41,7 @@ echo $resultArr[0];
     <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a>
     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 　</div>
+  <!-- This was made with GlassGenerator.netlify.app -->
+  <div class="glass-container" id="glass"></div>
   </body>
 </html>
